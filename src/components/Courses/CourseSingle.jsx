e66609dc69e6b701d1715e4a5a03cec30538d54e
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2022-10-27 01:22:20 +0530

    single course bug fix

--- src/components/Courses/CourseSingle.jsx
+++ src/components/Courses/CourseSingle.jsx
@@ -6,18 +6,17 @@ const CourseSingle = () => {
     return (
         <div className="flex flex-col max-w-5xl mx-auto overflow-hidden rounded">
             <img src={course.image} alt="" className="w-full h-60 sm:h-96 dark:bg-gray-500 rounded-lg shadow-lg" />
-            <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-5xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900 dark:text-white bg-white text-dark shadow-md">
+            <div className="p-6 pb-12 m-4 mx-auto -mt-16 space-y-6 lg:max-w-6xl sm:px-10 sm:mx-12 lg:rounded-md dark:bg-gray-900 dark:text-white bg-white text-dark shadow-md">
                 <div className="space-y-2">
                     <div className="inline-block text-2xl font-semibold sm:text-3xl">
                         {course.title}
 
                     </div>
-                    <p className="text-xs dark:text-gray-400">By
-                        <a rel="noopener noreferrer" href="#" className="text-xs hover:underline">Leroy Jenkins</a>
-                    </p>
                 </div>
-                <div className="dark:text-gray-100">
-                    <p>{course.description}</p>
+                <div className="dark:text-gray-100 ">
+                    <div className='flex flex-col space-y-4' dangerouslySetInnerHTML={{ __html: course.description }}>
+
+                    </div>
                 </div>
             </div>
         </div>
