2301a247134bca2d49ff3254a1288f705abfa863
Sandipan Das <87384281+sandidas@users.noreply.github.com>
2022-10-27 23:09:41 +0530

    Success! Thank You

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
 
             <section className="dark:bg-gray-800 dark:text-gray-100">
                 <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
-                    <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
+                    <div className="flex items-center justify-center p-6 mt-8">
                         <Player
                             src={animationData}
                             className=""
@@ -27,9 +27,80 @@ const Home = () => {
 
                         </p>
                         <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
-                            <Link to='/courses' className="px-8 py-3 text-lg font-semibold rounded dark:bg-violet-400 dark:text-gray-900">Courses</Link>
-                            <Link to='/blog' className="px-8 py-3 text-lg font-semibold border rounded dark:border-gray-100">Blog</Link>
+                            <Link to='/courses' className="px-8 py-3 space-x-4  rounded-md focus:ring-2 focus:ring-offset-1 dark:border-gray-400 focus:ring-violet-400 hover:bg-purple-800 hover:text-white bg-purple-600 text-white font-bold  text-lg">Courses</Link>
+                            <Link to='/blog' className="px-8 py-3 space-x-4 text-lg font-semibold border border-purple-600 rounded dark:border-gray-100 hover:bg-purple-800 hover:text-white">Blog</Link>
                         </div>
+                    </div>
+                </div>
+            </section>
+
+            <section className='grid grid-cols-1 lg:grid-cols-3 gap-5 mt-24'>
+                <div className="flex flex-col divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://sandipandas.net/assets/uploads/2020/07/aliabbaszadeh.jpg" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Devid Hillen</h4>
+                                <span className="text-xs dark:text-gray-400">2 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+                                <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+                            </svg>
+                            <span className="text-xl font-bold">4.9</span>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p> Fantastic experience learning with DegsKing great platform! Exactly what was envisioned. Looking forward to learning more in the future. Outstanding!</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://sandipandas.net/assets/uploads/2021/05/Avater-Male-O.jpg" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">5 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+                                <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+                            </svg>
+                            <span className="text-xl font-bold">5.0</span>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p> Over all excellent Learning with DegsKing, loved the work, very helping. My all future programing related learning are going to be done by this website.</p>
+                    </div>
+                </div>
+
+                <div className="flex flex-col divide-y rounded-md divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://sandipandas.net/assets/uploads/2020/07/amarafb2.jpg" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">1 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5 h-5 fill-current">
+                                <path d="M494,198.671a40.536,40.536,0,0,0-32.174-27.592L345.917,152.242,292.185,47.828a40.7,40.7,0,0,0-72.37,0L166.083,152.242,50.176,171.079a40.7,40.7,0,0,0-22.364,68.827l82.7,83.368-17.9,116.055a40.672,40.672,0,0,0,58.548,42.538L256,428.977l104.843,52.89a40.69,40.69,0,0,0,58.548-42.538l-17.9-116.055,82.7-83.368A40.538,40.538,0,0,0,494,198.671Zm-32.53,18.7L367.4,312.2l20.364,132.01a8.671,8.671,0,0,1-12.509,9.088L256,393.136,136.744,453.3a8.671,8.671,0,0,1-12.509-9.088L144.6,312.2,50.531,217.37a8.7,8.7,0,0,1,4.778-14.706L187.15,181.238,248.269,62.471a8.694,8.694,0,0,1,15.462,0L324.85,181.238l131.841,21.426A8.7,8.7,0,0,1,461.469,217.37Z"></path>
+                            </svg>
+                            <span className="text-xl font-bold">5.0</span>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p> We had a hard time understanding from other website as a beginning, but when we visit this website, it was a success, exactly what I was looking for.</p>
                     </div>
                 </div>
             </section>
